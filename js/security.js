4c5576d52f07c912bce035e2cc11f47cf3695379
robiulawal4k <robiulislam4797830@gmail.com>
2024-11-22 00:49:57 +0600

    first commit

---
+++ js/security.js
@@ -0,0 +1,3 @@
+document.getElementById('navigateToPage2').addEventListener('click', function (){
+    window.location.href = 'secority.html'; 
+})
--- js/web dev.js
+++ js/web dev.js
@@ -1,4 +1,7 @@
 document.getElementById('navigateToPage1').addEventListener('click', function (){
     window.location.href = 'signup.html'; 
+  
+
+  
 
 });
